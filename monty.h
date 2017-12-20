b1767aae057086eaed13033001a1ff46ad90e097
RoMalms10 <156@holbertonschool.com>
2017-12-20 04:43:35 +0000

    Add nop declaration

--- monty.h
+++ monty.h
@@ -61,5 +61,6 @@ void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 void find_op(void);
 #endif
