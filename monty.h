dd7c416eb88882bf37465977cd829e4ba4997410
RoMalms10 <156@holbertonschool.com>
2017-12-20 02:52:16 +0000

    Add function declarations

--- monty.h
+++ monty.h
@@ -58,5 +58,7 @@ void tokenize(char *buf);
 void free_token(void);
 void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 void find_op(void);
 #endif
