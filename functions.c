7bc83100ac20176bad624a86c05aa8012fb11d75
Vilyanare <acolyteofwar@gmail.com>
2017-12-20 02:43:16 +0000

    Added in pall and push

--- functions.c
+++ functions.c
@@ -1,4 +1,27 @@
 #include "monty.h"
+/**
+ * add_stack - add a node at the beggining of a stack_t
+ * @head: double pointer to head of list
+ * @n: number to add to n member
+ * Return: Address of new node or NULL if failed
+ */
+stack_t *add_stack(stack_t **head, const int n)
+{
+	stack_t *temp = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(stack_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->next = (*head == NULL) ? NULL : *head;
+	temp->prev = NULL;
+	temp->n = n;
+	if (*head)
+		(*head)->prev = temp;
+	*head = temp;
+	return (temp);
+}
 
 /**
   * tokenize - tokenizes a line into separate strings
@@ -30,4 +53,63 @@ void tokenize(char *buf)
 		token = strtok(NULL, delim);
 	}
 	/*vars->tokened = tokened;*/
+}
+
+/**
+ * pall - print all integers in a stack_t list
+ * @stack: NULL
+ * @line_number: 0
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head = vars->head;
+
+	while(head)
+	{
+		printf("%d\n", head->n);
+		head = head->next;
+	}
+
+}
+
+/**
+ * pall - print all integers in a stack_t list
+ * @stack: NULL
+ * @line_number: 0
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *newnode = NULL;
+	int i = 0, f = 0;
+	char *temp = vars->tokened[1];
+
+	newnode = add_stack(vars->head, 0);
+	if (newnode == NULL)
+	{
+		printf("Error: malloc failed");
+		free_token();
+		exit(EXIT_FAILURE);
+	}
+	if (temp)
+	{
+		while (temp[i])
+		{
+			if (isdigit(temp[i]) == 0)
+			{
+				f = 1;
+				break;
+			}
+			i++;
+		}
+		if (f = 0)
+		{
+			newnode->n = atoi(temp);
+			return;
+		}
+	}
+	printf("L%d: usage: push integer", vars->line_number);
+	free_token();
+	exit(EXIT_FAILURE);
+
+
 }
